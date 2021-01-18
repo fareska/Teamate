72c61afd25b470794346c254aa1f08b9dcb4cd0c
rmnaser90 <rmnaser90@gmail.com>
2021-01-18 18:52:46 +0200

    start working on seperate branch

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'mobx-react';
 import React from 'react';
-import { StyleSheet, Text, View, Button, SafeAreaView, ScrollView, Platform } from 'react-native';
+import { StyleSheet, Text, View, Button,Keyboard, SafeAreaView, ScrollView, Platform, TouchableWithoutFeedback } from 'react-native';
 import SignIn from './src/components/SignIn';
 
 import { styles as webStyles } from './src/styles/webStyles'
@@ -9,16 +9,15 @@ import { styles as mobileStyles } from './src/styles/mobileStyles'
 const styles = (Platform.OS === "web") ? webStyles : mobileStyles
 
 
-
 export default function App() {
   return (
-    <View style={styles.container}>
+    <TouchableWithoutFeedback style={styles.container} onPress={Keyboard.dismiss}>
       <SafeAreaView style={styles.container}>
        <SignIn/>
 
 
       </SafeAreaView>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button, SafeAreaView, ScrollView, Platform, TextInput, TouchableHighlight } from 'react-native';
+import { Text, View, Platform, TextInput, TouchableOpacity,  } from 'react-native';
+
+    import AsyncStorage from '@react-native-community/async-storage'
+
 import { styles as webStyles } from '../styles/webStyles'
 import { styles as mobileStyles } from '../styles/mobileStyles'
 const styles = (Platform.OS === "web") ? webStyles : mobileStyles
 
 const SignIn = () => {
+    // AsyncStorage.setItem('test','hello world!')
+    AsyncStorage.getItem('test').then(res => console.log(res))
     return (
         <View style={styles.signIn}>
+            
             <View style={styles.logoContainer}>
+                <View style={styles.circle}></View>
                 <Text style={styles.logo}>Teamate</Text>
             </View>
             <View style={styles.titleContainer}>
@@ -15,20 +22,33 @@ const SignIn = () => {
                 <Text style={styles.subTitle}>Hi There! Nice to see you.</Text>
             </View>
             <View style={styles.signInInputsContainer}>
-                <Text style={styles.label}>Email</Text>
-                <TextInput textContentType="email" style={styles.input}/>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.label}>Email</Text>
+                    <TextInput textContentType="username" style={styles.input} />
+                </View>
+                <View style={styles.inputContainer}>
                 <Text style={styles.label}>Password</Text>
-                <TextInput textContentType="password" secureTextEntry={true} />
+                <TextInput textContentType="password" style={styles.input} secureTextEntry={true} />
+            </View>
             </View>
             <View style={styles.signInBtnContainer}>
-                <TouchableHighlight>
-                    <View>
-                        <Text>
+                <TouchableOpacity style={styles.touchableButton} onPress={()=>console.log('touched')}>
+                    <View style={styles.signInBtn}>
+                        <Text  style={styles.btnText}>
                             Sign in
                         </Text>
                     </View>
-                </TouchableHighlight>
-            <Button style={styles.signInBtn} title="Sign in now"/>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.touchableButton} onPress={()=>console.log('touched')}>
+                    <View style={styles.signInBtnFacebook}>
+                        <Text style={styles.btnText}>
+                            Facebook
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>console.log('touched')} style={styles.signUpBtn}>
+                    <Text style={styles.sinUpBtnText}>Sign up</Text>
+                </TouchableOpacity>
             </View>
         </View>
     )
