54f4f483dad55dbc41b53576d3984cdde6fb22fb
HealthyJunkie <androwas101@gmail.com>
2021-01-25 01:18:29 +0200

    added map component

---
+++ src/components/Map.jsx
@@ -0,0 +1,78 @@
+import { inject, observer } from 'mobx-react';
+import React, { Component, useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Platform
+} from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+const map = inject('inputsStore')(observer((props) => {
+  const { newEventForm, handleTextInput } = props.inputsStore
+  const markerTA = {
+    latitude: newEventForm.lat,
+    longitude: newEventForm.lon,
+    latitudeDelta: newEventForm.lat,
+    longitudeDelta: newEventForm.lon
+  }
+
+  console.log(markerTA);
+  const onMapClick = e => {
+    const { latitude, longitude } = e.nativeEvent.coordinate
+    // console.log(e.nativeEvent.coordinate);
+    handleTextInput('newEventForm', 'lon', longitude)
+    handleTextInput('newEventForm', 'lat', latitude)
+    // setMarkerTA(e.nativeEvent)
+  }
+  const camera = {
+    center: {
+      latitude: props.event.latitude,
+      longitude: props.event.longitude,
+    },
+    pitch: 1,
+    heading: 1,
+
+    // Only on iOS MapKit, in meters. The property is ignored by Google Maps.
+    altitude: 1,
+
+    // Only when using Google Maps.
+    zoom: 10
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        ref={map => _map = map}
+        onPress={onMapClick}
+        showsUserLocation={true}
+        style={styles.map}
+        // initialRegion={props.event}
+        initialCamera={camera}
+      >
+
+        {props.isAdd ? props.events.map((p, index) => (
+          <Marker
+            key={index}
+            coordinate={p}
+            title={p.sport}
+            description={p.description}
+          />)) :
+          <Marker
+            coordinate={markerTA}
+            title={markerTA.title}
+            description={markerTA.description}
+          />}
+      </MapView>
+    </View>
+  );
+}))
+export default map
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
+});
